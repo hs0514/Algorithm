67480e07b9f89f04385dd7778e14066b74ac7c13
hs0514 <44011180+hs0514@users.noreply.github.com>
2023-01-10 17:43:55 +0900

    [level 1] Title: 문자열 다루기 기본, Time: 0.06 ms, Memory: 33.3 MB -BaekjoonHub

--- 프로그래머스/lv1/12918. 문자열 다루기 기본/문자열 다루기 기본.js
+++ 프로그래머스/lv1/12918. 문자열 다루기 기본/문자열 다루기 기본.js
@@ -5,3 +5,14 @@ function solution(s) {
   }
   return true
 }
+/*
+체크해야 될 것
+1. s의 length
+2. s를 number로 변환 후의 length (ex> 3e10)
+3. NaN
+4. s의 type
+
+2번에 걸려서 결국 문자 1개씩 체크
+소수일 경우도 생각 했었는데 제한사항에 소수의 예 (1.24) 같은 예시 없으니 주의
+제한사항 잘 보고 문제 풀기
+*/
