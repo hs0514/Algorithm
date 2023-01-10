03d9a1c7a632629c2607c6a03a53e896c885d5d6
hs0514 <44011180+hs0514@users.noreply.github.com>
2023-01-10 15:22:27 +0900

    [level 1] Title: 약수의 개수와 덧셈, Time: 0.28 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/lv1/77884. 약수의 개수와 덧셈/약수의 개수와 덧셈.js
+++ 프로그래머스/lv1/77884. 약수의 개수와 덧셈/약수의 개수와 덧셈.js
@@ -1,18 +1,12 @@
 function solution(left, right) {
+    // 코드 간결하게 수정
     let answer = 0;
-    let arr = [];
     for(let i = left; i <= right; i++) {
-      arr.push(i);
-    }; // 1. left, right 사이 값 구하기
-    for(let i = 0; i < arr.length; i++) {
       let divisor = 0; 
-      for(let j = 0; j <= arr[i]; j++) {
-//         console.log(arr[i]%j, arr[i]+"%"+j, arr[i]%j === 0)
-        if(arr[i]%j === 0) divisor++; // 2. 약수 개수 체크
+      for(let j = 0; j <= i; j++) {
+        if(i%j === 0) divisor++; // 약수 개수 체크
       };
-//       console.log(divisor, i, "약수의 수")
-      arr[i] = divisor%2 ? -arr[i] : arr[i]; // 3. 약수 개수가 홀수면 음수로 변환
-    }
-    arr.map(e => answer += e); // 4. 총 합 변수에 저장
+      answer += divisor%2 ? -i : i; // 약수 개수가 홀수면 음수로 변환 후 더해줌
+    }; 
     return answer;
 }
