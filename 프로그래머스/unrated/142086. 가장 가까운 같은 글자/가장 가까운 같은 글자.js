b8eb7e3aa88b826a45d1b7374094643c4cb8281b
hs0514 <44011180+hs0514@users.noreply.github.com>
2023-01-25 17:37:48 +0900

    [unrated] Title: 가장 가까운 같은 글자, Time: 4.23 ms, Memory: 37.8 MB -BaekjoonHub

--- 프로그래머스/unrated/142086. 가장 가까운 같은 글자/가장 가까운 같은 글자.js
+++ 프로그래머스/unrated/142086. 가장 가까운 같은 글자/가장 가까운 같은 글자.js
@@ -1,4 +1,21 @@
 function solution(s) {
-    var answer = [];
+    let answer = [];
+    for(let i = 0; i < s.length; i++) {
+      let j = i;
+      let num = 0;
+      let plusNum = 0;
+      let minusNum = -1;
+      do {
+        j--;
+        plusNum++;
+        if (s[i] === s[j]) {
+          num = plusNum;
+          break; // 같은 문자 나오면 종료
+        } else {
+          num = minusNum; // 같은 문자 없을 경우 계속 -1
+        }
+      } while (j > 0);
+      answer.push(num); // 문자 탐색 종료 후 배열에 push
+    }
     return answer;
 }
