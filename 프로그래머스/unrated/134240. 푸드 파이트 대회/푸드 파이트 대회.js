8e6867d714e1eb157e20debc61a0510755833949
hs0514 <44011180+hs0514@users.noreply.github.com>
2023-02-10 17:39:04 +0900

    [unrated] Title: 푸드 파이트 대회, Time: 0.05 ms, Memory: 33.6 MB -BaekjoonHub

--- 프로그래머스/unrated/134240. 푸드 파이트 대회/푸드 파이트 대회.js
+++ 프로그래머스/unrated/134240. 푸드 파이트 대회/푸드 파이트 대회.js
@@ -1,9 +1,4 @@
 function solution(food) {
-    let answer = '';
-    let arr = [];
-    for(let i = 1; i < food.length; i++) {
-      arr.push(String(i).repeat(food[i]/2))
-    }
-    answer = arr.join("") + '0' + arr.reverse().join("");
+    var answer = '';
     return answer;
 }
