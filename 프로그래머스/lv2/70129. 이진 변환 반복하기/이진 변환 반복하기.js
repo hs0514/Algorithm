81d56414d2c752e537f46b5d4f3642a92cd48a12
hs0514 <44011180+hs0514@users.noreply.github.com>
2023-02-24 17:41:05 +0900

    [level 2] Title: 이진 변환 반복하기, Time: 4.84 ms, Memory: 37.2 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/70129. 이진 변환 반복하기/이진 변환 반복하기.js
@@ -0,0 +1,18 @@
+function solution(n) {
+    let answer = 0;
+    let i = 0;
+    let zeroCnt = 0;
+    while(n !== "1") {
+      i++
+      if(n.length > 0 || n.includes("0")){
+        n = [...n].filter(e => {
+          if(e === "0") {
+            zeroCnt++
+          } else return e
+        }).length.toString(2)
+      } else {
+        n = "1"
+      }
+    }
+    return [i, zeroCnt]
+}
